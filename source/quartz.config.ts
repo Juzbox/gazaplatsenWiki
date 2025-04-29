fcefd5743c279ca4b376984e4d0ee396695993da
Hugo Pilate <hugopilate@gmail.com>
2025-04-29 23:55:19 +0200

    Update quartz.config.ts

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🍉",
+    pageTitle: "GAZAPLATSEN WIKI",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
