3f7a9dc39ad9cf571889af4798ff3bc60aa76cd2
Hugo Pilate <hugopilate@gmail.com>
2025-04-29 23:40:50 +0200

    Update quartz.layout.ts

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -17,9 +17,9 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
+   //Component.Breadcrumbs(),
     Component.ArticleTitle(),
-    Component.ContentMeta(),
+    //Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
@@ -28,7 +28,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     //Component.Search(),
     //Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.Explorer(),
   ],
   right: [
   //   //Component.DesktopOnly(Component.TableOfContents()),
