bb4dd4d19e47ce70df5f10368072514b8154325d
Hugo Pilate <hugopilate@gmail.com>
2025-05-06 12:19:04 +0200

    Update renderPage.tsx

--- source/quartz/components/renderPage.tsx
+++ source/quartz/components/renderPage.tsx
@@ -204,7 +204,14 @@ export function renderPage(
     </div>
   )
 
-
+const RightComponent = (
+    <div class="right sidebar">
+      {right.map((BodyComponent) => (
+        <BodyComponent {...componentData} />
+      ))}
+    </div>
+  )
+  
   const lang = componentData.fileData.frontmatter?.lang ?? cfg.locale?.split("-")[0] ?? "en"
   const doc = (
     <html lang={lang}>
@@ -234,7 +241,7 @@ export function renderPage(
                 ))}
               </div> }
             </div>
-            
+            {RightComponent}
             <Footer {...componentData} />
           </Body>
         </div>
