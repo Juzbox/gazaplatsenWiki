6453def7960d138e6bb9d76c517c982622af57b3
Hugo Pilate <hugopilate@gmail.com>
2025-06-01 01:46:54 +0200

    Update quartz.layout.ts

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -11,10 +11,10 @@ export const sharedPageComponents: SharedLayout = {
      
   ],
   footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
+    // links: {
+    //   GitHub: "https://github.com/jackyzha0/quartz",
+    //   "Discord Community": "https://discord.gg/cRFFHYye7t",
+    // },
   }),
 }
 
